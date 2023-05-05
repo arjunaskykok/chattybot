1f389748cf35b2bc915c97d7f31b115a8fcc2988
Arjuna Sky Kok <sky.kok@lezatify.com>
2023-05-05 23:55:45 +0700

    Add toast

--- app/src/main/java/com/kodeco/android/chattybot/ui/Settings.kt
+++ app/src/main/java/com/kodeco/android/chattybot/ui/Settings.kt
@@ -3,6 +3,7 @@ package com.kodeco.chattybot.ui
 import android.content.Intent
 import android.content.SharedPreferences
 import android.net.Uri
+import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,38 +16,44 @@ import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import com.kodeco.android.chattybot.R
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 @Composable
 fun SettingsScreen(sharedPreferences: SharedPreferences) {
   var text by remember { mutableStateOf(sharedPreferences.getString("OpenAI API Key", "") ?: "") }
   val context = LocalContext.current
+  val coroutineScope = rememberCoroutineScope()
+  val toastLabel = stringResource(R.string.toast_label)
   Column(
     modifier = Modifier
       .fillMaxSize()
       .padding(16.dp)
   ) {
     Text(
-      text = "Enter your text:",
+      text = stringResource(R.string.enter_api_key),
       style = MaterialTheme.typography.h6
     )
     TextField(
       value = text,
       onValueChange = { text = it },
       placeholder = {
-        Text(text = "Type here")
+        Text(text = stringResource(R.string.api_key_placeholder))
       },
       modifier = Modifier
         .fillMaxWidth()
         .padding(vertical = 8.dp)
     )
     Text(
-      text = "Get OpenAI API key",
+      text = stringResource(R.string.openai_api_key_link),
       style = MaterialTheme.typography.body2,
       color = MaterialTheme.colors.primary,
       modifier = Modifier
         .clickable {
-          val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://platform.openai.com/account/api-keys"))
+          val intent =
+            Intent(Intent.ACTION_VIEW, Uri.parse("https://platform.openai.com/account/api-keys"))
           context.startActivity(intent)
         }
         .padding(vertical = 8.dp)
@@ -55,12 +62,15 @@ fun SettingsScreen(sharedPreferences: SharedPreferences) {
       onClick = {
         val sharedPrefs = sharedPreferences
         sharedPrefs.edit().putString("OpenAI API Key", text).apply()
+        coroutineScope.launch {
+            Toast.makeText(context, toastLabel, Toast.LENGTH_SHORT).show()
+        }
       },
       modifier = Modifier
         .fillMaxWidth()
         .padding(vertical = 8.dp)
     ) {
-      Text(text = "Save")
+      Text(text = stringResource(R.string.save_button))
     }
   }
 }
