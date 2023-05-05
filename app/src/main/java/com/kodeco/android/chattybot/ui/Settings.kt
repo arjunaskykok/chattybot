ea5d6ed5cf8d4032119f5587ebbbf4c26816e031
Arjuna Sky Kok <sky.kok@lezatify.com>
2023-05-05 22:58:04 +0700

    Add URL to link

--- app/src/main/java/com/kodeco/android/chattybot/ui/Settings.kt
+++ app/src/main/java/com/kodeco/android/chattybot/ui/Settings.kt
@@ -1,6 +1,8 @@
 package com.kodeco.chattybot.ui
 
+import android.content.Intent
 import android.content.SharedPreferences
+import android.net.Uri
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,12 +14,13 @@ import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun SettingsScreen(sharedPreferences: SharedPreferences) {
   var text by remember { mutableStateOf(sharedPreferences.getString("OpenAI API Key", "") ?: "") }
-
+  val context = LocalContext.current
   Column(
     modifier = Modifier
       .fillMaxSize()
@@ -38,11 +41,14 @@ fun SettingsScreen(sharedPreferences: SharedPreferences) {
         .padding(vertical = 8.dp)
     )
     Text(
-      text = "Forgot your text?",
+      text = "Get OpenAI API key",
       style = MaterialTheme.typography.body2,
       color = MaterialTheme.colors.primary,
       modifier = Modifier
-        .clickable { /* do something */ }
+        .clickable {
+          val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://platform.openai.com/account/api-keys"))
+          context.startActivity(intent)
+        }
         .padding(vertical = 8.dp)
     )
     Button(
