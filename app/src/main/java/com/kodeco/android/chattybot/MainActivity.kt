bba4ed8283c324290e0509e362c470997fc5fdd1
Arjuna Sky Kok <sky.kok@lezatify.com>
2023-05-07 15:12:48 +0700

    Remove preview

--- app/src/main/java/com/kodeco/android/chattybot/MainActivity.kt
+++ app/src/main/java/com/kodeco/android/chattybot/MainActivity.kt
@@ -60,16 +60,3 @@ class MainActivity : ComponentActivity() {
     }
   }
 }
-
-@Composable
-fun Greeting(name: String) {
-  Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-  chattybotTheme {
-    Greeting("Android")
-  }
-}
